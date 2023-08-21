428bd2eb2a33b3072fafd5f78e9ecb364340443d
Alcassa <galcassa49@gmail.com>
2023-08-21 17:11:03 -0300

    Inserindo peças no tabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -8,11 +8,19 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8,8);
-            tab.colocarPeca( new Torre(tab,Cor.Amarelo) ,new Posicao(0, 0));
-            tab.colocarPeca( new Torre(tab,Cor.Preto) ,new Posicao(0, 7));
-            tab.colocarPeca(new Rei(tab,Cor.Branco) ,new Posicao(0, 2));
-            Tela.imprimirTabuleiro(tab);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+
+                tab.colocarPeca(new Torre(tab, Cor.Amarelo), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 9));
+                tab.colocarPeca(new Rei(tab, Cor.Branco), new Posicao(0, 2));
+
+                Tela.imprimirTabuleiro(tab);
+            }catch(TabuleiroExeption e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
---
+++ Xadrez/tabuleiro/TabuleiroExeption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xadrez.tabuleiro
+{
+    internal class TabuleiroExeption: Exception
+    {
+        public TabuleiroExeption(string msg):base(msg) { }
+    }
+}
